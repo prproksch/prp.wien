47009010da0c934b1cf3b8cdd447a1fe3bd25c5a
Philipp Proksch <mail@prp.wien>
2021-11-27 09:14:44 +0100

    🍱 added unity and csharp

--- data/techStackData.js
+++ data/techStackData.js
@@ -8,9 +8,10 @@ const techStackData = [
   { title: 'Node.js', image: '/static/images/stack/nodejs.svg' },
   { title: 'Git', image: '/static/images/stack/git.svg' },
   { title: 'GitLab', image: '/static/images/stack/gitlab.svg' },
-  { title: 'GraphQL', image: '/static/images/stack/graphql.svg' },
+  { title: 'C#', image: '/static/images/stack/c-sharp.svg' },
   { title: 'MySQL', image: '/static/images/stack/mysql.svg' },
   { title: 'TailwindCss', image: '/static/images/stack/tailwindcss.svg' },
+  { title: 'Unity2D', image: '/static/images/stack/unity.svg' },
   { title: 'Linux', image: '/static/images/stack/linux-tux.svg' },
 ]
 
