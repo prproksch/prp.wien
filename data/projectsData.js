54dd4b5a099aab1adf501bc44631fe616d938e10
Philipp R. Proksch <mail@prp.wien>
2023-04-04 11:56:43 +0200

    added VH and LG

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,4 +1,16 @@
 const projectsData = [
+   {
+    title: 'arm.letztegeneration.at',
+    description: `The Austrian branch of the Letztegeneration uses the tool to manage their activists. Using T3Stack, consisting of  Next.js, Typescript, Tailwind and tRPC.`,
+    imgSrc: '/static/images/projects/letztegeneration.jpg',
+    href: 'https://www.arm.letztegeneration.at',
+  },
+  {
+    title: 'veganhacktivists.org',
+    description: `I'm a developer on the VeganHacktivists Team. A global community of passionate animal advocates offering our skills in building technology for the animal protection movement. Using T3Stack, consisting of  Next.js, Typescript, Tailwind and tRPC.`,
+    imgSrc: '/static/images/projects/veganhacktivists.jpg',
+    href: 'https://www.veganhacktivists.org',
+  },
   {
     title: 'Herbertkoch.art',
     description: `The Website of Herbert Koch, an Artist living in Lower-Austria. Using HTML, CSS and JavaScript, with a bit of jQuerry, it is one of the first sites I ever created. `,
@@ -6,17 +18,17 @@ const projectsData = [
     href: 'https://www.Herbertkoch.art',
   },
   {
-    title: 'Eastside84.com',
-    description: `A webiste for Eastside84, a car tuning business located near Vienna. It was created using Wordpress and Woocommerce.`,
-    imgSrc: '/static/images/projects/eastside84.jpg',
-    //href: 'https://eastside84.com',
-  },
-  {
     title: 'Mfc-hohenau.at',
     description: `A Model RC Plane Club Mfc-Hohenau located in Lower-Austria. The site was created using HTML, CSS and JavaScript, and a bit of jQuerry.`,
     imgSrc: '/static/images/projects/mfchohenau.jpg',
     href: 'https://mfc-hohenau.at',
   },
+  {
+    title: 'Eastside84.com (deprecated)',
+    description: `A webiste for Eastside84, a car tuning business located near Vienna. It was created using Wordpress and Woocommerce.`,
+    imgSrc: '/static/images/projects/eastside84.jpg',
+    //href: 'https://eastside84.com',
+  }
 ]
 
 export default projectsData
