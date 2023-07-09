2861ab171e79f9c00c9c62b870e4f92934e308cd
Philipp R. Proksch <work@prp.codes>
2023-07-09 13:23:09 +0200

    removed Typing

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,22 @@
 import { PageSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
-import Typing from '@/components/Typing'
 import SocialIcon from '@/components/social-icons'
 export default function Home() {
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
-      <div className="divide-y divide-gray-200 dark:divide-gray-700 flex">
-        <section className="h-full flex flex-col justify-center w-full text-center mt-48	">
-          <h1 className="text-3xl md:text-7xl font-bold">
+      <div className="flex divide-y divide-gray-200 dark:divide-gray-700">
+        <section className="flex flex-col justify-center w-full h-full mt-48 text-center ">
+          <h1 className="text-3xl font-bold md:text-7xl">
             Hi, I'm Philipp!
             {/* <Typing /> */}
           </h1>
           <div></div>
           {/* <h2 className="text-l md:text-4xl">Let's write good software!</h2> */}
-          <div className="flex mt-4 space-x-4 justify-center content-center">
+          <div className="flex content-center justify-center mt-4 space-x-4">
             <SocialIcon kind="github" href={siteMetadata.github} size="6" />
             <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
-            <span role="img" aria-label="wave" className="wave text-2xl">
+            <span role="img" aria-label="wave" className="text-2xl wave">
               👋
             </span>
             <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
