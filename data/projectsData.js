e2ed4cb395de8a3271e42b16036848a7d5e3ec4c
Philipp R. Proksch <philipp.proksch@shiffable.com>
2024-01-01 19:27:23 +0100

    added crypthicc and donttellmeitdoesnotfit to projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,13 +1,19 @@
 const projectsData = [
-   {
-    title: 'arm.letztegeneration.at',
-    description: `The Austrian branch of the Letztegeneration uses the tool to manage their activists. Using T3Stack, consisting of  Next.js, Typescript, Tailwind and tRPC.`,
-    imgSrc: '/static/images/projects/letztegeneration.jpg',
-    //href: 'https://www.arm.letztegeneration.at',
+  {
+    title: 'donttellmeitdoesnotfit.com',
+    description: `Don't Tell Me It Does Not Fit - Find your ideal condom size without sifting through charts. Built using TypesSript, Next.js, shadcn/ui and Supabase`,
+    imgSrc: '/static/images/projects/donttellmeitdoesnotfit.jpg',
+    href: 'https://donttellmeitdoesnotfit.com',
+  },
+  {
+    title: 'crypthi.cc',
+    description: `A thicc wall of cryptography - that stands between your messages and the world. Built using TypesSript, TailwindCSS, SvelteKit and Supabase`,
+    imgSrc: '/static/images/projects/crypthicc.jpg',
+    href: 'https://www.crypthi.cc',
   },
   {
     title: 'veganhacktivists.org',
-    description: `I'm a developer on the VeganHacktivists Team. A global community of passionate animal advocates offering our skills in building technology for the animal protection movement. Using T3Stack, consisting of  Next.js, Typescript, Tailwind and tRPC.`,
+    description: `I'm a developer on the VeganHacktivists Team, a global community of passionate animal advocates. Using Next.js, Typescript, TailwindCSS and tRPC.`,
     imgSrc: '/static/images/projects/veganhacktivists.jpg',
     href: 'https://www.veganhacktivists.org',
   },
@@ -28,7 +34,7 @@ const projectsData = [
     description: `A webiste for Eastside84, a car tuning business located near Vienna. It was created using Wordpress and Woocommerce.`,
     imgSrc: '/static/images/projects/eastside84.jpg',
     //href: 'https://eastside84.com',
-  }
+  },
 ]
 
 export default projectsData
